393f2e34f50f9800c2d21bf4dbbb0aebb96e69f3
Yehonal <hw.2@hotmail.it>
2014-12-10 11:17:38 +0100

    push files

---
+++ NodeJs.js
@@ -0,0 +1,16 @@
+/*
+ * Copyright (C) 2007 - 2014 Hyperweb2 All rights reserved.
+ * GNU General Public License version 3; see www.hyperweb2.com/terms/
+ */
+
+'use strict';
+
+hw2.define([
+    "hw2!PATH_JS_LIB:nodejs/common/include.js",
+    "hw2!PATH_JS_LIB:common/index.js"
+], function () {
+    var $ = this;
+    $.NodeJs = $.Class({});
+
+    return $.NodeJs;
+}); 
