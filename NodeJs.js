20133512ce3833b82cf278da62e2939e1089dcb4
Yehonal <hw.2@hotmail.it>
2015-04-26 17:36:06 +0200

    Updated all repositories

--- NodeJs.js
+++ NodeJs.js
@@ -6,8 +6,8 @@
 'use strict';
 
 hw2.define([
-    "hw2!PATH_JS_LIB:nodejs/common/include.js",
-    "hw2!PATH_JS_LIB:common/index.js"
+    "hw2!{PATH_JS_LIB}nodejs/common/include.js",
+    "hw2!{PATH_JS_LIB}common/index.js"
 ], function () {
     var $ = this;
     $.NodeJs = $.Class({});
--- index.js
+++ index.js
@@ -6,8 +6,8 @@
 'use strict';
 
 hw2.define([
-    'hw2!PATH_JS_LIB:nodejs/common/include.js',
-    'hw2!PATH_JS_LIB:nodejs/common/NodeJs.js'
+    'hw2!{PATH_JS_LIB}nodejs/common/include.js',
+    'hw2!{PATH_JS_LIB}nodejs/common/NodeJs.js'
 ],function () {
     var $ = this;
 });
